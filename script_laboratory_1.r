bc580fdd5fa3acdfc64b5bc318f5e8e64c9bc336
juanesklk <juan.viera@uao.edu.co>
2026-02-24 12:26:37 -0500

    Se cambiaron las rutas absolutas por relativas

--- script_laboratory_1.r
+++ script_laboratory_1.r
@@ -4,11 +4,14 @@
 # Usa el directorio actual como directorio raiz, evitando escribir setwd()
 
 library(here)
+
+here()
+
 # -------------------------------------------------------------------------
 # CASO 1
 # -------------------------------------------------------------------------
 
-#
+# 
 
 # -------------------------------------------------------------------------
 # Analisis exploratorio
@@ -26,6 +29,23 @@ df_2024_sem1 = read.csv("data/Caso_1/rendimiento_2024_sem1.csv")
 df_2024_sem2 = read.csv("data/Caso_1/rendimiento_2024_sem2.csv")
 df_2025_sem1 = read.csv("data/Caso_1/rendimiento_2025_sem1.csv")
 df_2025_sem2 = read.csv("data/Caso_1/rendimiento_2025_sem2.csv")
+
+archivos <- list(
+  df_2021_sem1,
+  df_2021_sem2,
+  df_2022_sem1,
+  df_2022_sem2,
+  df_2023_sem1,
+  df_2023_sem2,
+  df_2024_sem1,
+  df_2024_sem2,
+  df_2025_sem1,
+  df_2025_sem2
+)
+
+columnas <- lapply(archivos, colnames)
+
+all(sapply(columnas, function(x) identical(x, columnas[[1]])))
 
 # revisamos los archivos manualmente para verificar que tuviesen las mismas
 # columnas.
